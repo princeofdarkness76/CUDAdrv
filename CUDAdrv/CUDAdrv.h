2355f0b2a9f629087e25981eeba9c36009d6ff60
Deiter <deiter@Deiters-MacBook-Pro.local>
2015-08-17 06:32:42 -0700

    Initial Commit
    update CUDA

---
+++ CUDAdrv/CUDAdrv.h
@@ -0,0 +1,13 @@
+//
+//  CUDAdrv.h
+//  CUDAdrv
+//
+//  Created by Deiter on 2015-08-17.
+//  Copyright (c) 2015 SJ Technical. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CUDAdrv : NSObject
+
+@end
---
+++ CUDAdrv/CUDAdrv.m
@@ -0,0 +1,13 @@
+//
+//  CUDAdrv.m
+//  CUDAdrv
+//
+//  Created by Deiter on 2015-08-17.
+//  Copyright (c) 2015 SJ Technical. All rights reserved.
+//
+
+#import "CUDAdrv.h"
+
+@implementation CUDAdrv
+
+@end
